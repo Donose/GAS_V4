f6c729b7a45e84707946ec4d3c5b21fd13ac7390
Donose <54245480+Donose@users.noreply.github.com>
2019-10-08 21:57:09 +0300

    dfg
    added 50 page checks

--- Keywords/checkers/TextCheckOF.groovy
+++ Keywords/checkers/TextCheckOF.groovy
@@ -27,67 +27,153 @@ public class TextCheckOF {
 		if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME1'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF1'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME2'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME2'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF2'))
-		} else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME3'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME3'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF3'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME4'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME4'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF4'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME5'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME5'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF5'))
-		} else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME6'), 'From ' + findTestData('Test Data').getValue(
+		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME6'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF6'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME7'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME7'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF7'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME8'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME8'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF8'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME9'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME9'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF9'))
-		} else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME10'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME10'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF10'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME11'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME11'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF11'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME12'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME12'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF12'))
-		} else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME13'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME13'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF13'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME14'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME14'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF14'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME15'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME15'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF15'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME16'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME16'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF16'))
-		} else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME17'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME17'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF17'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME18'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME18'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF18'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME19'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME19'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF19'))
-		} else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME20'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME20'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF20'))
-		}else if ( WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME21'), 'From ' + findTestData('Test Data').getValue(
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME21'), 'From ' + findTestData('Test Data').getValue(
 		1, 1),FailureHandling.OPTIONAL) == true){
 			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF21'))
-		}
-		
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME22'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF22'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME23'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF23'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME24'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF24'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME25'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF25'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME26'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF26'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME27'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF27'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME28'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF28'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME29'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF29'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME30'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF30'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME31'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF31'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME32'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF32'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME33'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF33'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME34'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF34'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME35'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF35'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME36'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF36'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME37'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF37'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME38'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF38'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME39'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF39'))
+		} else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME40'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF40'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME41'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF41'))
+	    }else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME42'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF42'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME43'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF43'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME44'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF44'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME45'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF45'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME46'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF46'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From M47'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF47'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME48'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF48'))
+		} else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME49'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF49'))
+		}else if (WebUI.verifyElementText(findTestObject('Link Processes/ME to SO/span_From ME50'), 'From ' + findTestData('Test Data').getValue(
+		1, 1),FailureHandling.OPTIONAL) == true){
+			WebUI.click(findTestObject('Link Processes/ME to SO/button_OF50'))
+	    }
 	}
 }
---
+++ Object Repository/Link Processes/ME to SO/button_OF40.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_OF21</name>
+   <tag></tag>
+   <elementGuidId>3ceb2619-3a59-4763-a085-a453ee01629f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[@type='button'])[91]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;mainData&quot;)/table[@class=&quot;table table-bordered table-striped&quot;]/tbody[1]/tr[40]/td[1]/div[@class=&quot;btn-group-xs pull-center&quot;]/button[@class=&quot;btn btn-info pull-center&quot;][count(. | //*[@type = 'button' and (contains(text(), '
+                            OF') or contains(., '
+                            OF'))]) = count(//*[@type = 'button' and (contains(text(), '
+                            OF') or contains(., '
+                            OF'))])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-if</name>
+      <type>Main</type>
+      <value>processData.offer_id &amp;&amp; !processData.team_management_id &amp;&amp; !processData.products_order_id &amp;&amp; !processData.contract_id</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-info pull-center</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-ng-click</name>
+      <type>Main</type>
+      <value>openOffer(processData.offer_id);</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            OF</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;mainData&quot;)/table[@class=&quot;table table-bordered table-striped&quot;]/tbody[1]/tr[21]/td[1]/div[@class=&quot;btn-group-xs pull-center&quot;]/button[@class=&quot;btn btn-info pull-center&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>(//button[@type='button'])[91]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='mainData']/table/tbody/tr[8]/td/div/button</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='N/A'])[36]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='N/A'])[35]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='From ME-401'])[1]/preceding::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Offer'])[8]/preceding::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[8]/td/div/button</value>
+   </webElementXpaths>
+</WebElementEntity>
